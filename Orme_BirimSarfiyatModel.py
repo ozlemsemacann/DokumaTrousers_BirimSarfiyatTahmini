425f16a5a343dd4c627d90cbe7964b16c20f1c12
ozlemsemacann <ozlem.semacan@defacto.com>
2025-11-04 15:49:13 +0300

    Update Orme_BirimSarfiyatModel.py

--- Orme_BirimSarfiyatModel.py
+++ Orme_BirimSarfiyatModel.py
@@ -16,9 +16,9 @@ model = load_model()
 # -----------------------------
 # Streamlit Arayuzu
 # -----------------------------
-st.title("?? Birim Sarfiyat Tahmini")
+st.title("🧵 Birim Sarfiyat Tahmini")
 
-st.markdown("Modeli onceden e?ittik ve yukledik. ?imdi de?erleri gir, tahmini al!")
+st.markdown("Modeli onceden egittik ve yukledik. Simdi degerleri gir, tahmini al!")
 
 # Kullan?c?dan giri?ler
 inputs = {}
@@ -60,4 +60,5 @@ if st.button("Tahmin Et"):
 
     X_new_pool = Pool(X_new, cat_features=cat_features)
     prediction = model.predict(X_new_pool)[0]
-    st.success(f"?? Tahmini Birim Sarfiyat: **{prediction:.2f}**")
+
+    st.success(f"🔮 Tahmini Birim Sarfiyat: **{prediction:.2f}**")
